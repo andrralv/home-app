7adaddc52dfaaa4e9f7be1ac765dca16415bc3df
andre <andrralv@gmail.com>
2018-09-30 14:29:30 -0600

    andre/feature: responsive media queries. UI IS FINISHED

--- ui/src/components/Review.js
+++ ui/src/components/Review.js
@@ -1,16 +1,29 @@
 import React from 'react';
+import Modal from 'react-modal';
 import  './SignIn.css';
 
 class Review extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            modalIsOpen: false
+          };
         this.submit = this.submit.bind(this);
+        this.openModal = this.openModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
+    }
+
+openModal() {
+    this.setState({modalIsOpen: true});
+    }
+
+    closeModal() {
+    this.setState({modalIsOpen: false});
     }
 
 submit(event) {
-    event.preventDefault();
-    this.props.changeFormData(this.state);
-}
+    alert('jajaj conoce')
+    }
 
 render() {
     return (
@@ -22,10 +35,32 @@ render() {
                 <h4>{`Phone Number: ${this.props.data.phone}`}</h4>
                 <h4>{`Salary Range: ${this.props.data.salary}`}</h4>
             </div>
-            <h2 onClick={this.submit} className={'form-submit-button'}>Submit</h2>
-        </div>
+            <h2 onClick={this.openModal} className={'form-submit-button'}>Submit</h2>
+            <Modal
+                isOpen={this.state.modalIsOpen}
+                onAfterOpen={this.afterOpenModal}
+                onRequestClose={this.closeModal}
+                style={customStyles}
+                contentLabel="Example Modal"
+                >
+                You're all Done!
+            </Modal>
+            </div>
         );
     }
 }
+
+const customStyles = {
+    content : {
+      top                   : '50%',
+      left                  : '50%',
+      right                 : 'auto',
+      bottom                : 'auto',
+      marginRight           : '-50%',
+      transform             : 'translate(-50%, -50%)',
+      background            : '#282828',
+      color                 : 'white',
+    }
+  };
 
 export default Review;
